71e1c0f8c59fbe2537f9429e40deda46a76f9207
BullTheRockLord <timrmiles@gmail.com>
2020-10-14 22:57:11 +0000

    Created bot cooldown method, and made it threaded Added logic for bot playing audio when changing channels Added ability for bot to play audio

--- bot.py
+++ bot.py
@@ -3,105 +3,111 @@ import discord
 import asyncio
 import time
 import pathlib
+import constants
+import threading
 
-
-
-SONG_FOLDER="songs"
-
-
+#Load the sound library
 if not discord.opus.is_loaded():
-    discord.opus.load_opus('opus')
+ discord.opus.load_opus('libopus.so');
 
-print("Opus loaded")
-
+#declare our discord client
 client = discord.Client()
 
-#Define a cooldown class
+#___HELPER FUNCTIONS____
+def song_exists(id):
+    filename = "song-" + str(id)
+    return os.path.exists(os.path.join(constants.UPLOAD_FOLDER,filename)+".mp3")
+def get_song_name(id):
+    return os.path.join(constants.UPLOAD_FOLDER,"song-" + str(id))+".mp3"
 
-class Cooldown:
-    
-    time = 0
-    id = 0
-    
-    def __init__(self, idIn):
+#___COOLDOWN FUNCTIONS___
+users_in_cooldown = {}
+cooldown_lock = False
+def update_user_cooldown(id):
+    if str(id) not in users_in_cooldown:
+        users_in_cooldown[str(id)] = constants.START_COOLDOWN
+    elif users_in_cooldown[str(id)] == 0:
+        users_in_cooldown[str(id)] = constants.START_COOLDOWN
+    else:
+        users_in_cooldown[str(id)] = users_in_cooldown[str(id)] * constants.COOLDOWN_MULTPLIER
+        if users_in_cooldown[str(id)] < constants.MIN_COOLDOWN :
+                users_in_cooldown[str(id)] = constants.MIN_COOLDOWN
 
-        #Implemet cooldown blacklist
+def update_all_cooldowns():
+    for id, cooldown in users_in_cooldown.items():
+        if cooldown > 0:
+            print("Cooldown for user " + id + " is " + str(cooldown - 1))
+            users_in_cooldown[id] = cooldown - 1
 
-        id = idIn
-        time = time.time()
+def cooldown_loop():
+    while True:
+        time.sleep(1)
+        cooldown_lock = True
+        update_all_cooldowns()
+        cooldown_lock = False
 
-    def cooldownExpired(self):
+def get_cooldown(id):
+    if str(id) not in users_in_cooldown:
+        return 0
+    return users_in_cooldown[str(id)];
 
-        if time.time() - time > 300:
-            return True
-        else:
-            return False
-
-#Utilty functions
-
-def songExists(id):
-   
-    id = "song-" + str(id)
-
-    for i in os.listdir(SONG_FOLDER):
-        path = pathlib.Path("songs/" + i)
-        print(path.stem)
-        if path.stem == str(id):
-            return True
-      
-
-    return False
-
-def handleCooldown(id):
-    return 1 #NOT IMPlEMENTED YET
-
-#Event handlers
+#___DISCORD EVENT HANDLERS____
 @client.event
 async def on_ready():
-    print("logged in as " + client.user.name)
-    print("id" + str(client.user.id))
-
+    print("Ready to go.")
+    print("Logged in as " + client.user.name)
 
 @client.event
-async def on_voice_state_update(member, before, after):
-    
-    print(str(member.display_name) + " changed voice state")
+async def on_voice_state_update(user, before, after):
 
-    #Begin logic checks to determine what to do
+    #If this was non channel update, then no purpose doing anything
+    if before.channel == after.channel:
+        return
 
-    #if we are no longer in a channel, then bot does not do anything
-    if after.channel == None:
-        return None
+    #If there is no song, there is no point doing all this logic
+    if not song_exists(user.id):
+        return 
 
-    #if we were in a channel before, then we switched channel, and don't need to do anything
-
-    if before.channel != None:
-        return None
-
-    #Now that we have joined the channel, we must check if we have a song to play for this user
-
-    if not songExists(member.id):
-        return None
-
-    #We have a song, but are we out of cooldown
-
-    if handleCooldown(member.id) == False:
-        return None
-    
-    #Okay, we now know we can play the song
-    
-    voiceClient = await after.channel.connect()
-
-    audioPlayer = voiceClient.create_ffmpeg_player('songs/songs' + str(member.id))
-
-    audioPlayer.start()
-
-    while not auidoPlayer.is_done():
+    #await release of cooldown lock
+    while cooldown_lock:
         pass
 
-    await voiceClient.disconnect()
+    #If the player is leaving a channel
+    if after.channel == None:
+        update_user_cooldown(user.id);
+        return
+     
+    #if the player is entering a channel
+    if before.channel == None and after.channel != None:
+        if get_cooldown(user.id) != 0:
+            print("Not playing song for user " + user.name + " becuase of cooldown")
+            update_user_cooldown(user.id)
+            return
+        update_user_cooldown(user.id)
 
-    #FUTURE, WHAT IF MULTIPLE CHANNELS EXPERINCE A JOIN ACROSS ONE GUILD?
+        print("Playing user song for " + user.name);
 
+        voice_client = await after.channel.connect()
+        voice_client.play(discord.FFmpegPCMAudio(get_song_name(user.id)))
 
-client.run(os.environ.get('BOTTOKEN'))
+        abort_time = 0
+        while voice_client.is_playing() :
+            time.sleep(0.1)
+            abort_time += 0.1
+            if(abort_time > constants.ALLOWED_LENGTH):
+                print("Was forced to abort playing song for " + user.name)
+                break
+
+        await voice_client.disconnect()
+
+    #if the player is switching channels, then we do nothing
+    if before.channel != None and after.channel != None:
+        pass
+
+#__START COOLDOWN LOOP__
+cooldown_thread = threading.Thread(target=cooldown_loop, args=(), daemon=True)
+cooldown_thread.start()
+
+#___RUN DISCORD CLIENT___
+client.run(constants.BOT_TOKEN)
+
